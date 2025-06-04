4c1276d4cf94cbed314bbaf5fa24ccb241c0359f
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-06-04 12:54:03 +0700

    update

--- Trainer/train.py
+++ Trainer/train.py
@@ -28,7 +28,9 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    
+    train_ds = None
+    eval_ds = None
+    trainer=None
     # clone once
     local = snapshot_download(repo_id=args.repo, token=args.hf_token)
     ckpt = find_checkpoint(local)
